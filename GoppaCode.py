ec5cca15828aa039c57190ac5e27ca0ec1b231e3
d-laslo <ice_bear@ukr.net>
2021-11-21 11:03:48 +0200

    chng Patterson parameters

--- GoppaCode.py
+++ GoppaCode.py
@@ -2,7 +2,9 @@ from numpy.lib.function_base import append
 from source import *
 import numpy as np
 from copy import copy
-from random import randint
+# from random import randint
+import random
+import os
 
 class GoppaCode:
     def __init__(self, p, g, n = 0):
@@ -10,11 +12,11 @@ class GoppaCode:
         self.__n = n
 
         self.__t = len(g) - 1
-        self.__r = self.__t * 2
+        self.__r = self.__t
         self.__m = len(bin(self.__p)[2:]) - 1
         
         if self.__n != 0:
-            self.__k = self.__n - self.__r
+            self.__k = self.__n - self.__m * self.__t
             self.__d = self.__n - self.__k + 1
 
             self.__L = self.__gen_L()
@@ -22,7 +24,8 @@ class GoppaCode:
             self.__g = self.__get_g(g)
             self.__H = self.__gen_H()
             self.__G = self.__gen_G()
-            # t = mul_matrix(self.__H, self.__G, self.__p)
+            # self.__H = self.__tmp_gen_H()
+            # t = np.dot(self.__H, np.array(self.__G).T) % 2
             # tt = 0
 
     @property
@@ -86,12 +89,8 @@ class GoppaCode:
     
     def __get_g(self, g):
         g.pop(-1)
-        gg = []
-        for i in g:
-            gg.append(i)
-            gg.append(0)
-        gg.append(self.__mul(self.__prime_element, self.__prime_element))
-        return gg
+        g.append(self.__prime_element)
+        return g
 
 
     def __mul(self, x1, x2):
@@ -150,23 +149,8 @@ class GoppaCode:
     def __gen_G(self):
         bH = convert_to_binary_matrix(self.__H, self.__m)
 
-        # is_transpose = False
-        
-
-        # if len(bH) > len(bH[0]):
-        #     bH = transpose(bH)
-        #     is_transpose = True
-        #     # gt_size = self.__k
-        #     # gt_size_element = self.__n
-
         gt_size = len(bH[0])
         gt_size_element = self.__k
-
-        # for row in H_T:
-        #     brow = ['0' * (self.__m - len(bin(el)[2:])) + bin(el)[2:] for el in row]
-        #     bH_T.append(list(''.join(brow)))
-
-        # bH = list(np.array(bH_T).transpose())
 
         equations = []
         for row in bH:
@@ -188,10 +172,13 @@ class GoppaCode:
 
         tt = len(bin(equations[0])[2:]) - 1
         equations = equations[::-1]
+        # equations = equations
         max_num = 2**gt_size_element - 1 #2**(self.__m * self.__r) - 1
+        
         while True:
-            # G_T = [0 for i in range(gt_size)]
-            G_T =  [2**i for i in range(self.__k)][::-1] + [0 for i in range(gt_size - self.__k)]
+            # # eye = [] #[2**i for i in range(self.__k)]
+            G_T = [0 for i in range(gt_size)]
+            # G_T =  [2**i for i in range(self.__k)][::-1] + [0 for i in range(gt_size - self.__k)]
             for itr in equations:
                 indexes = get_indexes(itr)
                 for i in range(len(indexes)):
@@ -205,13 +192,22 @@ class GoppaCode:
 
                 if len(null_indexes) == 0:
                     continue
+                
+                #!######################################
+                # remove_ind = []
+                # if len(eye) > 0 and len(null_indexes) > 1:
+                #     for j in range(1, len(null_indexes)):
+                #         G_T[null_indexes[j]] = eye.pop(0)
+                #         remove_ind.append(j)
+                # remove_ind = remove_ind[::-1]
+                # for i in remove_ind:
+                #     null_indexes.pop(i)     
 
-                # if len(null_indexes) == 1:
-                #     G_T[null_indexes[0]] = -1
-                #     continue
+                #!######################################
                 
                 for i in null_indexes:
-                    G_T[i] = randint(1, max_num)
+                    random.seed(os.urandom(15))
+                    G_T[i] = random.randint(1, max_num)
 
                 # перший незаповнений елемент є сумою всіх інших елементів у строкі
                 res = 0
@@ -230,7 +226,7 @@ class GoppaCode:
                 if G_T[null_indexes[0]] == 0:
                     tmp = G_T[null_indexes[1]]
                     while tmp == G_T[null_indexes[1]]:
-                        tmp = randint(1, max_num)
+                        tmp = random.randint(1, max_num)
 
                     G_T[null_indexes[0]] = G_T[null_indexes[1]] ^ tmp
                     G_T[null_indexes[1]] = tmp
@@ -267,7 +263,9 @@ class GoppaCode:
             # GGT = split(G_T, self.__k)
             # tt = np.dot(bH, GGT) %2
             G = binary_transpose(G_T, self.__k)
+            
             # tt = split(G, self.__n)
+            GG = split(G, self.__n)
             if min(G) == 0:
                 continue
             #     if is_transpose:
@@ -277,6 +275,135 @@ class GoppaCode:
             return split(G, self.__n)
 
 
+    # def __tmp_gen_H(self):
+    #     bH = self.__G #convert_to_binary_matrix(self.__H, self.__m)
+
+    #     gt_size = len(bH[0])
+    #     gt_size_element = self.__r * self.__m
+
+    #     equations = []
+    #     for row in bH:
+    #         equations.append(int(''.join([str(em) for em in row]), 2))
+
+    #     # приводимо систему рівнянь до трапецеїдального вигляду
+    #     for i in range(len(equations)):
+    #         # max_element = max(equations[i:])
+    #         ind = index_max_len(equations[i:]) + i
+    #         max_element = equations[ind]
+    #         len_max_element = len(bin(max_element)) - 2
+
+    #         equations[equations.index(max_element)] = equations[i]
+    #         equations[i] = max_element
+
+    #         for j in range(i + 1, len(equations)):
+    #             if equations[j] > 0 and len_max_element == (len(bin(equations[j])) - 2):
+    #                 equations[j] ^= max_element
+
+    #     tt = len(bin(equations[0])[2:]) - 1
+    #     equations = equations[::-1]
+    #     # equations = equations
+    #     max_num = 2**gt_size_element - 1 #2**(self.__m * self.__r) - 1
+        
+    #     while True:
+    #         # eye = [] #[2**i for i in range(self.__k)]
+    #         G_T = [0 for i in range(gt_size)]
+    #         # G_T =  [2**i for i in range(self.__k)][::-1] + [0 for i in range(gt_size - self.__k)]
+    #         for itr in equations:
+    #             indexes = get_indexes(itr)
+    #             for i in range(len(indexes)):
+    #                 indexes[i] = tt - indexes[i]
+
+    #             # знаходимо індекси всіх незаповнених елементів матриці G
+    #             null_indexes = []
+    #             for i in indexes:
+    #                 if G_T[i] == 0:
+    #                     null_indexes.append(i)
+
+    #             if len(null_indexes) == 0:
+    #                 continue
+                
+    #             #!######################################
+    #             # remove_ind = []
+    #             # if len(eye) > 0 and len(null_indexes) > 1:
+    #             #     for j in range(1, len(null_indexes)):
+    #             #         G_T[null_indexes[j]] = eye.pop(0)
+    #             #         remove_ind.append(j)
+    #             # remove_ind = remove_ind[::-1]
+    #             # for i in remove_ind:
+    #             #     null_indexes.pop(i)     
+
+    #             #!######################################
+                
+    #             for i in null_indexes:
+    #                 random.seed(os.urandom(15))
+    #                 G_T[i] = random.randint(1, max_num)
+
+    #             # перший незаповнений елемент є сумою всіх інших елементів у строкі
+    #             res = 0
+    #             for i in indexes:
+    #                 if i == null_indexes[0]:
+    #                     continue
+    #                 res ^= G_T[i]
+    #             G_T[null_indexes[0]] = res if res > 0 else G_T[null_indexes[0]]
+
+
+    #             # якщо сума всіх елементів, окрім першого незаповненого рівняється 0,
+    #             # тоді має існувати, як мінімум два незаповнених елементи, 
+    #             # у такому разі достатньо перегенерувати другий незаповнений елемент, 
+    #             # і перерахувати перший незаповнений елемент
+
+    #             if G_T[null_indexes[0]] == 0:
+    #                 tmp = G_T[null_indexes[1]]
+    #                 while tmp == G_T[null_indexes[1]]:
+    #                     tmp = random.randint(1, max_num)
+
+    #                 G_T[null_indexes[0]] = G_T[null_indexes[1]] ^ tmp
+    #                 G_T[null_indexes[1]] = tmp
+
+    #             # indexes = get_indexes(itr)
+    #             # for i in range(len(indexes)):
+    #             #     indexes[i] = tt - indexes[i]
+    #             sum = 0
+    #             for i in indexes:
+    #                 sum ^= G_T[i]
+    #             ttt = 0
+
+    #         def split(matrix, k):
+    #             reslt_matrix = []
+    #             _len = k
+    #             for row in matrix:
+    #                 row = '0' * (_len - len(bin(row)[2:])) + bin(row)[2:]
+    #                 reslt_matrix.append([int(i, 2) for i in list(row)])
+    #             return reslt_matrix
+
+
+    #         # bed_ind = []
+    #         # # G_T = G_T
+    #         # for ind in range(len(bH)):
+    #         #     sum = 0
+    #         #     for i in bH[ind]:
+    #         #         if i:
+    #         #             sum ^= G_T[i]
+    #         #     if sum !=0:
+    #         #         bed_ind.append(ind)
+    #         # tt = 0
+
+
+    #         # GGT = split(G_T, self.__k)
+    #         # tt = np.dot(bH, GGT) %2
+    #         G = binary_transpose(G_T, gt_size_element)
+            
+    #         # tt = split(G, self.__n)
+    #         GG = split(G, gt_size)
+    #         if min(G) == 0:
+    #             continue
+    #         #     if is_transpose:
+    #         #         gg = split(G_T, 12)
+    #         #         return gg
+    #         #     gg = 
+    #         return split(G, gt_size)
+
+
 
 if __name__ == '__main__':
-   GoppaCode(19, [1,1,1], 16)
+   GoppaCode(19, [1,1,1], 12)
